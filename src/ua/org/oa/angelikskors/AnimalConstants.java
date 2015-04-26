5873a2523c74191dd06084e2d5acb22f4a3131b6
Mykola Dymura <nikolay.dymura@gmail.com>
2015-04-26 13:30:09 +0300

    Comments to code.

--- src/ua/org/oa/angelikskors/AnimalConstants.java
+++ src/ua/org/oa/angelikskors/AnimalConstants.java
@@ -7,3 +7,21 @@ public interface AnimalConstants {
 	int RABBIT = 3;
 	int NOANIMAL = 4;
 }
+//declaring interface that contains only constants it's a bad style
+//if you need type that contains only constants use class or enum
+/*
+class AnimalConstants {
+
+	private AnimalConstants(){
+		//declare constructor as public, because you need only constants so instance of class should not be created
+	}
+	public static final int ALPACA = 0;
+	public static final int DOG = 1;
+	public static final int LEMUR = 2;
+	public static final int RABBIT = 3;
+	public static final int NO_ANIMAL = 4;// split name of variable using underscore in constants
+}*/
+
+/*enum Animals{
+	ALPACA, DOG, LEMUR, RABBIT, NOANIMAL
+}*/
--- src/ua/org/oa/angelikskors/CanProtect.java
+++ src/ua/org/oa/angelikskors/CanProtect.java
@@ -1,5 +1,5 @@
 package ua.org.oa.angelikskors;
-
+//not good name for interface. In that case name of interface should be answer on question WHO? - Protector
 public interface CanProtect {
 
 	void attack();
